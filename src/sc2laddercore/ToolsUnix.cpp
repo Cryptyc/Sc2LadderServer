9377f10ef9d1a71bb0389fbfba6c14222e92c070
m1ndgames <m1ndfuck@m1ndfuck.de>
2018-07-07 21:15:32 +0200

    https://github.com/Cryptyc/Sc2LadderServer/pull/65/commits/cf297727fb62b31f6adbe61213739b876693fdf6

--- src/sc2laddercore/ToolsUnix.cpp
+++ src/sc2laddercore/ToolsUnix.cpp
@@ -15,67 +15,73 @@
 
 void StartBotProcess(const BotConfig &Agent, const std::string &CommandLine, unsigned long *ProcessId)
 {
-    pid_t pID = fork();
+	pid_t pID = fork();
 
-    if (pID < 0)
-    {
-        std::cerr << Agent.BotName + ": Can't fork the bot process, error: " +
-            strerror(errno) << std::endl;
-        return;
-    }
+	if (pID < 0)
+	{
+		std::cerr << Agent.BotName + ": Can't fork the bot process, error: " +
+			strerror(errno) << std::endl;
+		return;
+	}
 
-    if (pID == 0) // child
-    {
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
+	if (pID == 0) // child
+	{
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
 
-        // FIXME (alkurbatov): Redirect stderr to a file like ToolsWindows do.
-        //close(STDERR_FILENO);
+		// FIXME (alkurbatov): Redirect stderr to a file like ToolsWindows do.
+		//close(STDERR_FILENO);
 
-        int ret = chdir(Agent.RootPath.c_str());
-        if (ret < 0) {
-            std::cerr << Agent.BotName +
-                ": Can't change working directory to " + Agent.RootPath +
-                ", error: " + strerror(errno) << std::endl;
-            exit(errno);
-        }
+		int ret = chdir(Agent.RootPath.c_str());
+		if (ret < 0) {
+			std::cerr << Agent.BotName +
+				": Can't change working directory to " + Agent.RootPath +
+				", error: " + strerror(errno) << std::endl;
+			exit(errno);
+		}
 
-        std::vector<char*> unix_cmd;
-        std::istringstream stream(CommandLine);
-        std::istream_iterator<std::string> begin(stream), end;
-        std::vector<std::string> tokens(begin, end);
-        for (const auto& i : tokens)
-            unix_cmd.push_back(const_cast<char*>(i.c_str()));
+		std::vector<char*> unix_cmd;
+		std::istringstream stream(CommandLine);
+		std::istream_iterator<std::string> begin(stream), end;
+		std::vector<std::string> tokens(begin, end);
+		for (const auto& i : tokens)
+			unix_cmd.push_back(const_cast<char*>(i.c_str()));
 
-        // FIXME (alkurbatov): Unfortunately, the cmdline uses relative path.
-        // This hack is needed because we have to change the working directory
-        // before calling to exec.
-        if (Agent.Type == BinaryCpp)
-            unix_cmd[0] = const_cast<char*>(Agent.FileName.c_str());
+		// FIXME (alkurbatov): Unfortunately, the cmdline uses relative path.
+		// This hack is needed because we have to change the working directory
+		// before calling to exec.
+		if (Agent.Type == BinaryCpp)
+			unix_cmd[0] = const_cast<char*>(Agent.FileName.c_str());
 
-        unix_cmd.push_back(NULL);
+		unix_cmd.push_back(NULL);
 
-        ret = execv(unix_cmd.front(), &unix_cmd.front());
-        if (ret < 0)
-        {
-            std::cerr << Agent.BotName + ": Failed to execute '" + CommandLine +
-                "', error: " + strerror(errno) << std::endl;
-            exit(errno);
-        }
+		// NOTE (alkurbatov): For the Python bots we need to search in the PATH
+		// for the interpreter.
+		if (Agent.Type == Python)
+			ret = execvp(unix_cmd.front(), &unix_cmd.front());
+		else
+			ret = execv(unix_cmd.front(), &unix_cmd.front());
 
-        exit(0);
-    }
+		if (ret < 0)
+		{
+			std::cerr << Agent.BotName + ": Failed to execute '" + CommandLine +
+				"', error: " + strerror(errno) << std::endl;
+			exit(errno);
+		}
 
-    // parent
-    *ProcessId = pID;
+		exit(0);
+	}
 
-    int exit_status = 0;
-    int ret = waitpid(pID, &exit_status, 0);
-    if (ret < 0) {
-        std::cerr << Agent.BotName +
-            ": Can't wait for the child process, error:" +
-            strerror(errno) << std::endl;
-    }
+	// parent
+	*ProcessId = pID;
+
+	int exit_status = 0;
+	int ret = waitpid(pID, &exit_status, 0);
+	if (ret < 0) {
+		std::cerr << Agent.BotName +
+			": Can't wait for the child process, error:" +
+			strerror(errno) << std::endl;
+	}
 }
 
 void StartExternalProcess(const std::string CommandLine)
@@ -85,29 +91,29 @@ void StartExternalProcess(const std::string CommandLine)
 
 void SleepFor(int seconds)
 {
-    sleep(seconds);
+	sleep(seconds);
 }
 
 void KillSc2Process(unsigned long pid)
 {
-    int ret = kill(pid, SIGKILL);
-    if (ret < 0)
-    {
-        std::cerr << std::string("Failed to send SIGKILL, error:") +
-            strerror(errno) << std::endl;
-    }
+	int ret = kill(pid, SIGKILL);
+	if (ret < 0)
+	{
+		std::cerr << std::string("Failed to send SIGKILL, error:") +
+			strerror(errno) << std::endl;
+	}
 }
 
 bool MoveReplayFile(const char* lpExistingFileName, const  char* lpNewFileName)
 {
-    int ret = rename(lpExistingFileName, lpNewFileName);
-    if (ret < 0)
-    {
-        std::cerr << std::string("Failed to move a replay file, error:") +
-            strerror(errno) << std::endl;
-    }
+	int ret = rename(lpExistingFileName, lpNewFileName);
+	if (ret < 0)
+	{
+		std::cerr << std::string("Failed to move a replay file, error:") +
+			strerror(errno) << std::endl;
+	}
 
-    return ret == 0;
+	return ret == 0;
 }
 
 #endif
