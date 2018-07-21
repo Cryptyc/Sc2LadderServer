e4c2885082b8392095086c0741606c49a13c28da
m1ndgames <m1ndfuck@m1ndfuck.de>
2018-07-21 11:03:03 +0200

    fix indentation again

--- src/sc2laddercore/ToolsUnix.cpp
+++ src/sc2laddercore/ToolsUnix.cpp
@@ -15,59 +15,59 @@
 
 void StartBotProcess(const BotConfig &Agent, const std::string &CommandLine, unsigned long *ProcessId)
 {
-	pid_t pID = fork();
+pid_t pID = fork();
 
-	if (pID < 0)
-	{
-		std::cerr << Agent.BotName + ": Can't fork the bot process, error: " +
-			strerror(errno) << std::endl;
-		return;
+if (pID < 0)
+{
+	std::cerr << Agent.BotName + ": Can't fork the bot process, error: " +
+		strerror(errno) << std::endl;
+	return;
+}
+
+if (pID == 0) // child
+{
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+
+	// FIXME (alkurbatov): Redirect stderr to a file like ToolsWindows do.
+	//close(STDERR_FILENO);
+
+	int ret = chdir(Agent.RootPath.c_str());
+	if (ret < 0) {
+		std::cerr << Agent.BotName +
+			": Can't change working directory to " + Agent.RootPath +
+			", error: " + strerror(errno) << std::endl;
+		exit(errno);
 	}
 
-	if (pID == 0) // child
+	std::vector<char*> unix_cmd;
+	std::istringstream stream(CommandLine);
+	std::istream_iterator<std::string> begin(stream), end;
+	std::vector<std::string> tokens(begin, end);
+	for (const auto& i : tokens)
+		unix_cmd.push_back(const_cast<char*>(i.c_str()));
+
+	// FIXME (alkurbatov): Unfortunately, the cmdline uses relative path.
+	// This hack is needed because we have to change the working directory
+	// before calling to exec.
+	if (Agent.Type == BinaryCpp)
+		unix_cmd[0] = const_cast<char*>(Agent.FileName.c_str());
+
+	unix_cmd.push_back(NULL);
+
+	// NOTE (alkurbatov): For the Python bots we need to search in the PATH
+	// for the interpreter.
+	if (Agent.Type == Python || Agent.Type == Wine || Agent.Type == Mono)
+		ret = execvp(unix_cmd.front(), &unix_cmd.front());
+	else
+		ret = execv(unix_cmd.front(), &unix_cmd.front());
+
+	if (ret < 0)
 	{
-		close(STDIN_FILENO);
-		close(STDOUT_FILENO);
-
-		// FIXME (alkurbatov): Redirect stderr to a file like ToolsWindows do.
-		//close(STDERR_FILENO);
-
-		int ret = chdir(Agent.RootPath.c_str());
-		if (ret < 0) {
-			std::cerr << Agent.BotName +
-				": Can't change working directory to " + Agent.RootPath +
-				", error: " + strerror(errno) << std::endl;
-			exit(errno);
-		}
-
-		std::vector<char*> unix_cmd;
-		std::istringstream stream(CommandLine);
-		std::istream_iterator<std::string> begin(stream), end;
-		std::vector<std::string> tokens(begin, end);
-		for (const auto& i : tokens)
-			unix_cmd.push_back(const_cast<char*>(i.c_str()));
-
-		// FIXME (alkurbatov): Unfortunately, the cmdline uses relative path.
-		// This hack is needed because we have to change the working directory
-		// before calling to exec.
-		if (Agent.Type == BinaryCpp)
-			unix_cmd[0] = const_cast<char*>(Agent.FileName.c_str());
-
-		unix_cmd.push_back(NULL);
-
-		// NOTE (alkurbatov): For the Python bots we need to search in the PATH
-		// for the interpreter.
-		if (Agent.Type == Python || Agent.Type == Wine || Agent.Type == Mono)
-			ret = execvp(unix_cmd.front(), &unix_cmd.front());
-		else
-			ret = execv(unix_cmd.front(), &unix_cmd.front());
-
-		if (ret < 0)
-		{
-			std::cerr << Agent.BotName + ": Failed to execute '" + CommandLine +
-				"', error: " + strerror(errno) << std::endl;
-			exit(errno);
-		}
+		std::cerr << Agent.BotName + ": Failed to execute '" + CommandLine +
+			"', error: " + strerror(errno) << std::endl;
+		exit(errno);
+	}
 
 		exit(0);
 	}
