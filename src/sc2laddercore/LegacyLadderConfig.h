cb60b14e7f6234b1c402c3bf637407b9f8913e01
lladdy <randallboland@bigpond.com>
2018-08-04 09:59:28 +0930

    Save current config code as legacy

---
+++ src/sc2laddercore/LegacyLadderConfig.h
@@ -0,0 +1,17 @@
+#pragma once
+class LegacyLadderConfig
+{
+public:
+    LegacyLadderConfig(const std::string &InConfigFile);
+    bool ParseConfig();
+    bool WriteConfig();
+    std::string GetValue(std::string RequestedValue);
+    void AddValue(const std::string &Index, const std::string &Value);
+private:
+    static void TrimString(std::string &Str);
+    const std::string ConfigFileLocation;
+    std::map<std::string, std::string> options;
+
+
+};
+
