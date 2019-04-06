e5debb26721fa72b40b2982793bd1bc3691aa570
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2019-04-06 22:21:23 +0300

    Better travis CI
    * Compile for different modes.
    * Add support of windows CI.
    
    Signed-off-by: Alexander Kurbatov <alkurbatov@virtuozzo.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get install -y g++-5
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
-else
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     # Apply compilation fixes for OS X.
     git apply hacks/civetweb_compilation_fix.patch
 fi
